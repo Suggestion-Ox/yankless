6699d2e9d2443a786a8fbc29bb390687dbee587d
Andrew Berkowitz <andrew@andrewberkowitz.com>
2023-02-26 19:08:05 -0500

    Update script

--- yankless.js
+++ yankless.js
@@ -1,50 +1,50 @@
 class Yankless {
   constructor(options = {}) {
+    const DIALECTS = ["GB", "AU", "NZ", "ZA", "IE"];
+    const DEFAULT_DEBUG_COLOR = "red";
+    const DEFAULT_FROM = "US";
+
     this.options = options;
     this.debug = options.debug || window.location.search.indexOf("debug") > -1;
-    this.from = options.from || "US";
-    this.debugColor = options.debugColor || "red";
-    this.dialects = options.dialects || ["GB", "AU", "NZ", "ZA", "IE"];
+    this.from = options.from || DEFAULT_FROM;
+    this.debugColor = options.debugColor || DEFAULT_DEBUG_COLOR;
+    this.dialects = options.dialects || DIALECTS;
     this.disable = true;
     this.add = options.add || {};
     this.ignore = options.ignore || [];
+    this.translations = {};
     this.loadDictionary();
   }
 
-  loadDictionary() {
+  async loadDictionary() {
     // Get the language from the browser
     var language = navigator.language.split("-");
     // If the language is not English and the browser is not in the dialects list, don't load the dictionary
     if (!(language[0] == "en" && this.dialects.includes(language[1]))) return;
     // Load the dictionary
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "/assets/js/translations.js";
-    script.defer = true;
-    document.head.appendChild(script);
-    script.onload = () => {
-      // If we've set the from option to a dialect, swap the keys and values so we're translating to American
-      if (this.dialects.includes(this.from)) {
-        var r = Object.fromEntries(
-          Object.entries(translations).map(([key, value]) => [value, key])
-        );
-        translations = r;
-      }
-      // Add any additional words and remove any ignored words
-      if (this.add) {
-        translations = { ...translations, ...this.add };
-      }
-      if (this.ignore) {
-        this.ignore.forEach((word) => delete translations[word]);
-      }
-      this.disable = false;
-    };
+    let { translations } = await import("./translations.js");
+    this.translations = translations;
+    // If we've set the from option to a dialect, swap the keys and values so we're translating to American
+    if (this.dialects.includes(this.from)) {
+      var r = Object.fromEntries(
+        Object.entries(this.translations).map(([key, value]) => [value, key])
+      );
+      this.translations = r;
+    }
+    // Add any additional words and remove any ignored words
+    if (this.add) {
+      this.translations = { ...this.translations, ...this.add };
+    }
+    if (this.ignore) {
+      this.ignore.forEach((word) => delete this.translations[word]);
+    }
+    this.disable = false;
   }
 
   replaceWordforNode(c) {
     var debug = this.debug;
     var debugColor = this.debugColor;
-    var t = translations;
+    var t = this.translations;
     var r = new RegExp(`\\b(${Object.keys(t).join("|")})\\b`, "gi");
     if (c.nodeValue.match(r)) {
       var newString = c.nodeValue.replace(r, function (m) {
